a188204a0041438089d4dd165bc59611fbafef36
Rahulkrishna23 <83756065+Rahulkrishna23@users.noreply.github.com>
2021-05-06 14:40:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<title>PHP Quiz</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<p>PHP Quiz</p>
+		</div>
+	</header>
+
+	<main>
+    <div class = "menu">
+
+
+<a href="main.php" class="button">Take Quiz</a>
+
+</div>
+	</main>
+
+
+	<footer>
+			<div class="container">
+				
+			</div>
+
+
+	</footer>
+
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,47 @@
+<?php 
+include 'db.php';
+$query = "SELECT * FROM questions";
+$total_questions = mysqli_num_rows(mysqli_query($connection,$query));
+
+
+?>
+<html>
+<head>
+	<title>PHP Quiz</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<p>PHP Quiz</p>
+		</div>
+	</header>
+
+	<main>
+			<div class="container">
+				<h2>Test Your PHP Knowledge</h2>
+				<p>
+					This is a multiple choise quiz to test your PHP Knowledge.
+				</p>
+				<ul>
+					<li><strong>Number of Questions:</strong><?php echo $total_questions; ?> </li>
+					<li><strong>Type:</strong> Multiple Choise</li>
+					
+
+				</ul>
+
+				<a href="question.php?n=1" class="start">Start Quize</a>
+
+			</div>
+
+	</main>
+
+
+	<footer>
+			<div class="container">
+				
+			</div>
+
+
+	</footer>
